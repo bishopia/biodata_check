0f3b923ec5aa7eef7a3975388c568da8065c789f
Ian Bishop <iwbishop@gmail.com>
2017-06-14 22:24:25 -0600

    cleaning up comments and variable names

--- biodata_check.R
+++ biodata_check.R
@@ -1,14 +1,17 @@
-#Title: biodata_check() function
+#Title: biodata_check()
 #Author: Ian Bishop
-#Date: 10 June 2017
+#Date: 14 June 2017
 
-biodata_check <- function(list_filename="", data_filename="", biodata_filename="", data_type=c("List", "Dataframe")) {
+biodata_check <- function(list_filename="", 
+                          data_filename="", 
+                          biodata_filename="", 
+                          data_type=c("List", "Dataframe")) {
 
-  #required libraries
+  #Libraries
   library(qdapTools)
   library(reshape2)
   
-  #Import Biodata Complete Taxonomy  
+  #Import biodata complete taxonomy  
   biodata_names <- read.csv(paste(biodata_filename, ".csv", sep=""), header=TRUE, stringsAsFactors = FALSE, fileEncoding="UTF-8-BOM")
   
   
@@ -33,11 +36,12 @@ biodata_check <- function(list_filename="", data_filename="", biodata_filename="
     #Replace NAs in CurrentBiodataName column with original user name.
     conversion_df[is.na(conversion_df$CurrentBiodataName)==TRUE,]$CurrentBiodataName <- conversion_df[is.na(conversion_df$CurrentBiodataName)==TRUE,]$UserTaxa
   
-    #confirm
+    #Convert conversion_df to dataframe.
     conversion_df <- data.frame(lapply(conversion_df, as.character), stringsAsFactors=FALSE)
   
-    #export conversion_df to .csv
+    #Export conversion_df to .csv
     # write.csv(conversion_df, "OUTPUT_converted_names.csv", row.names = FALSE)
+    
     return(conversion_df)
   
   } else if(data_type=="Dataframe") {
@@ -45,34 +49,36 @@ biodata_check <- function(list_filename="", data_filename="", biodata_filename="
     #Import user wide format tidy dataset  
     user_data <- read.csv(paste(data_filename, ".csv", sep=""), check.names = FALSE, stringsAsFactors=FALSE, header=TRUE, fileEncoding="UTF-8-BOM")
     
-    #Add user list to new conversion dataframe
+    #Create local variable for imported data
     working_wide <- user_data
   
-    #convert to long format
+    #Convert working_wide to long format
     working_wide <- melt(working_wide, value.name="Count", variable.name="Taxon", id.vars="Sample")
   
-    #add BiodataTaxon column of current Biodata names
+    #Add BiodataTaxon column of current Biodata names
     working_wide$BiodataTaxon <- lookup_e(working_wide$Taxon, biodata_names[,c("BenchTaxonName", "BiodataTaxonName")])
   
   
     
-    #change sheet
-    change_sheet <- working_wide[,c("Taxon", "BiodataTaxon")]
-    change_sheet <- unique(change_sheet)
+    #Name translation sheet
+    working_translation <- working_wide[,c("Taxon", "BiodataTaxon")]
+    working_translation <- unique(working_translation)
+    
     #Add column for change notes
-    change_sheet$ConversionNotes <- NA
-    change_sheet[is.na(change_sheet$BiodataTaxon)==FALSE,]$ConversionNotes <- "Name Updated to Current Biodata Name"
-    change_sheet[is.na(change_sheet$BiodataTaxon)==TRUE,]$ConversionNotes <- "Name Not Found In Biodata"
-    change_sheet$ConversionNotes[as.character(change_sheet$BiodataTaxon)==as.character(change_sheet$Taxon)] <- "Name Already Current"
-    #fill NAs in change_sheet$BiodataTaxon with Taxon
-    change_sheet$BiodataTaxon[is.na(change_sheet$BiodataTaxon)] <- as.character(change_sheet$Taxon[is.na(change_sheet$BiodataTaxon)])
+    working_translation$ConversionNotes <- NA
+    working_translation[is.na(working_translation$BiodataTaxon)==FALSE,]$ConversionNotes <- "Name Updated to Current Biodata Name"
+    working_translation[is.na(working_translation$BiodataTaxon)==TRUE,]$ConversionNotes <- "Name Not Found In Biodata"
+    working_translation$ConversionNotes[as.character(working_translation$BiodataTaxon)==as.character(working_translation$Taxon)] <- "Name Already Current"
+   
+    #Fill NAs in working_translation$BiodataTaxon with Taxon
+    working_translation$BiodataTaxon[is.na(working_translation$BiodataTaxon)] <- as.character(working_translation$Taxon[is.na(working_translation$BiodataTaxon)])
   
   
     
     #Fill in BiodataTaxon NAs with original taxon.
     working_wide$BiodataTaxon[is.na(working_wide$BiodataTaxon)] <- as.character(working_wide$Taxon[is.na(working_wide$BiodataTaxon)])
     
-    #collected list of names that have been changed
+    #Collected list of names that have been changed
     changed_names <- as.data.frame(unique(working_wide[working_wide$Taxon != working_wide$BiodataTaxon,]$Taxon))
     colnames(changed_names) <- "Changed Names"
     
@@ -89,14 +95,14 @@ biodata_check <- function(list_filename="", data_filename="", biodata_filename="
     #Convert from long format back to wide format
     working_wide <- dcast(working_wide, Sample ~ Taxon)
     
-    #consider exporting this as XLSX with two datasheets
-    #export conversion_df to .csv
+    #Export working_wide to .csv
     # write.csv(working_wide, "OUTPUT_renamed_data.csv", row.names = FALSE)
     # #export changed name list
     # write.csv(changed_names, "OUTPUT_name_changes.csv", row.names = FALSE)
     
-    output<-list(UpdatedDataset = working_wide, ChangedNames = changed_names, NameTranslationSheet=change_sheet)
+    output<-list(UpdatedDataset = working_wide, ChangedNames = changed_names, NameTranslationSheet=working_translation)
     return(output)
-  
-  } else {return(print("ERROR: Please choose either 'List' or 'Dataframe' for data_type"))}
+    
+    #Return error message if data_type string is misspelled
+  } else { return(print("ERROR: Please choose either 'List' or 'Dataframe' for data_type"))}
 }
